7f5641614f6c7ca6c46572199fee46967e91342f
Rajendra <Rajendra@versionsolutions-mac-mini.local>
2018-10-05 11:49:08 +0530

    Initial Commit

---
+++ QRScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRScanner
+//
+//  Created by Rajendra on 05/10/18.
+//  Copyright © 2018 Rajendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
